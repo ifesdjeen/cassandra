3d52bb077f6f4661c1e465581c78121fb1adafcb
Alex Petrov <oleksandr.petrov@gmail.com>
2018-10-17 06:36:52 +0200

    Make Shared executor variable public

--- src/java/org/apache/cassandra/concurrent/SharedExecutorPool.java
+++ src/java/org/apache/cassandra/concurrent/SharedExecutorPool.java
@@ -57,7 +57,7 @@ import static org.apache.cassandra.concurrent.SEPWorker.Work;
 public class SharedExecutorPool
 {
 
-    static final SharedExecutorPool SHARED = new SharedExecutorPool("SharedPool");
+    public static final SharedExecutorPool SHARED = new SharedExecutorPool("SharedPool");
 
     // the name assigned to workers in the pool, and the id suffix
     final String poolName;
