a1285abcd0a33f02e5179402d6a48d9b0e93d414
Alex Petrov <oleksandr.petrov@gmail.com>
2019-02-05 13:40:20 +0100

    In JVM dtests need to clean up after instance shutdown
    Followup - clean up global buffer pool; disable for non-high capacity env
    
    patch by Alex Petrov; reviewed by Benedict for CASSANDRA-14922

--- test/distributed/org/apache/cassandra/distributed/impl/Instance.java
+++ test/distributed/org/apache/cassandra/distributed/impl/Instance.java
@@ -27,6 +27,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.function.BiConsumer;
@@ -357,9 +359,9 @@ public class Instance extends IsolatedExecutor implements IInvokableInstance
         }
     }
 
-    public void shutdown()
+    public Future<Void> shutdown()
     {
-        sync((ExecutorService executor) -> {
+        Future<?> future = async((ExecutorService executor) -> {
             Throwable error = null;
             error = parallelRun(error, executor,
                     Gossiper.instance::stop,
@@ -386,11 +388,14 @@ public class Instance extends IsolatedExecutor implements IInvokableInstance
                                 StageManager::shutdownAndWait,
                                 SharedExecutorPool.SHARED::shutdown
             );
+            BufferPool.globalPool.reset();
             LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
             loggerContext.stop();
-            super.shutdown();
             Throwables.maybeFail(error);
-        }).accept(isolatedExecutor);
+        }).apply(isolatedExecutor);
+
+        return CompletableFuture.runAsync(ThrowingRunnable.toRunnable(future::get))
+                                .thenRun(super::shutdown);
     }
 
     private static Throwable parallelRun(Throwable accumulate, ExecutorService runOn, ThrowingRunnable ... runnables)
@@ -424,10 +429,5 @@ public class Instance extends IsolatedExecutor implements IInvokableInstance
             }
         }
         return accumulate;
-    }
-
-    public static interface ThrowingRunnable
-    {
-        public void run() throws Throwable;
     }
 }
