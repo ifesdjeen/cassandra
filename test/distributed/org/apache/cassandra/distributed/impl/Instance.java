9277ab635f435a67eabb7b0a7d9283ba29bb6848
Alex Petrov <oleksandr.petrov@gmail.com>
2019-11-07 14:28:07 -0800

    Since this method was previously used only in `callsOnInstance` context, we didn’t really need to wrap it, but this is not the case anymore

--- test/distributed/org/apache/cassandra/distributed/impl/Instance.java
+++ test/distributed/org/apache/cassandra/distributed/impl/Instance.java
@@ -146,9 +146,11 @@ public class Instance extends IsolatedExecutor implements IInvokableInstance
     @Override
     public UUID schemaVersion()
     {
-        // we do not use method reference syntax here, because we need to sync on the node-local schema instance
-        //noinspection Convert2MethodRef
-        return Schema.instance.getVersion();
+        return callsOnInstance(() -> {
+            // we do not use method reference syntax here, because we need to sync on the node-local schema instance
+            //noinspection Convert2MethodRef
+            return Schema.instance.getVersion();
+        }).call();
     }
 
     public void startup()
